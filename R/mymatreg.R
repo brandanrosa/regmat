c206d5c34e393e529d2e641e92b6ce46ff2d06d6
Brandan Rosa <brandanrosa22@gmai.com>
2024-06-11 20:02:41 -0500

    Second

--- R/mymatreg.R
+++ R/mymatreg.R
@@ -1,17 +1,27 @@
+#' mymatreg
+#'
+#' A function which returns many great things from matrix algebra for regression
+#'
+#' @param obj an object from an `lm()`
+#'
+#' @return an invisible list to call including: summary, data frame, characteristic & response matrices, beta estimates, RSS, ssq, s, fitted values, and residuals.
+#' @export
+#'
+#' @examples \dontrun{mymatreg(ylm)}
 mymatreg <- function(obj) { # obj = lm() object
 
   # summary
   sm <- summary(obj)
 
   # data frame
-  dat <- model.frame(obj)
+  dat <- stats::model.frame(obj)
 
   # n and k
   n <- dim(dat)[1]
   k <- dim(dat)[2] - 1
 
   # matrices
-  X <- model.matrix(obj)
+  X <- stats::model.matrix(obj)
   Y <- dat[, 1]
   Y <- as.matrix(Y)
 
@@ -25,4 +35,19 @@ mymatreg <- function(obj) { # obj = lm() object
   ssq <- RSS / (n - (k + 1))
   s <- sqrt(ssq)
 
+  # fitted and residuals
+  yhat <- X %*% betahat
+  res <- Y - yhat
+
+  # list
+  invisible(list(summary=sm,
+                 dat=dat,
+                 X=X,
+                 Y=Y,
+                 betahat=betahat,
+                 RSS=RSS,
+                 ssq=ssq,
+                 s=s,
+                 fit=yhat,
+                 resid=res))
 }
